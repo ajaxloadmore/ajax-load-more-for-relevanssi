e9d578250ec6dc11ca8ac351b65068a0b52cb2db
Darren Cooney <darren.cooney@gmail.com>
2021-04-13 10:10:11 -0400

    Adding support for term highlighting.

--- README.txt
+++ README.txt
@@ -4,7 +4,7 @@ Plugin URI: https://connekthq.com/plugins/ajax-load-more/extensions/relevanssi/
 Donate link: https://connekthq.com/donate/
 Tags: ajax load more, search, relevanssi, search results, filter, ajax, infinite scroll
 Requires at least: 3.6
-Tested up to: 5.3.1
+Tested up to: 5.7.0
 Stable tag: 1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -25,18 +25,18 @@ The extension works by providing a connection point between Ajax Load More and R
 
 == Frequently Asked Questions ==
 
-
 = How does this work? =
 This extensions works by using the [alm_query_args](https://connekthq.com/plugins/ajax-load-more/docs/filter-hooks/#alm_query_args) filter to pass values to the Relevanssi query and then back to Ajax Load More. 
-
 
 = How do I pass a search term to Relevanssi =
 You can pass search term to your Ajax Load More shortcode. [ajax_load_more search="My Search Query" id="relevanssi"]
 
-
+= How do I highlight the search term in the search excerpts? =
+In your Ajax Load More Repeater Template you can do the following using the [relevanssi_do_excerpt](https://github.com/msaari/relevanssi/blob/master/lib/excerpts-highlights.php#L27) function.
+	global $post;
+	echo relevanssi_do_excerpt( $post, $args['search'] );
 
 == Screenshots ==
-
 
 
 == Installation ==
@@ -55,15 +55,12 @@ You can pass search term to your Ajax Load More shortcode. [ajax_load_more searc
 4. Ensure Ajax Load More is installed prior to activating the plugin.
 5. Activate the plugin in the WP plugin dashboard.
 
-
 == Changelog ==
 = 1.0.1 - December 18, 2019=
 * NEW - Added support for core Relevanssi filter [relevanssi_modify_wp_query](https://www.relevanssi.com/knowledge-base/ordering-search-results-date/)
 
-
 = 1.0 =
 * Initial Release.
-
 
 == Upgrade Notice ==
 * None 
--- ajax-load-more-for-relevanssi.php
+++ ajax-load-more-for-relevanssi.php
@@ -10,26 +10,32 @@ Author URI: https://connekthq.com
 Version: 1.0.1
 License: GPL
 Copyright: Darren Cooney & Connekt Media
-*/
 
+// @codingStandardsIgnoreStart
+*/
 
 if(!class_exists('ALM_Relevanssi')) :   
    
+	/**
+	 * ALM Relevanssi Class.
+	 * 
+	 * @author ConnektMedia <darren@connekthq.com>
+	 */
    class ALM_Relevanssi{	   
       
+		/**
+		 * Construct Relevanssi class.
+		 */
    	function __construct(){	
          add_filter('alm_relevanssi', array(&$this, 'alm_relevanssi_get_posts'), 10, 1);	
-      }
+      }           
       
-      
-      
-      /*
-   	*  alm_relevanssi_get_posts
-   	*  Get relevanssi search results and return post ids in post__in wp_query param
-   	*
-   	*  @return $args
-   	*  @since 1.0
-   	*/
+      /**
+   	 * Get relevanssi search results and return post ids in post__in wp_query param.
+   	 *
+   	 * @return $args
+   	 * @since  1.0
+   	 */
       function alm_relevanssi_get_posts($args){
  
       	if(function_exists('relevanssi_do_query')){         	
@@ -40,14 +46,15 @@ if(!class_exists('ALM_Relevanssi')) :
          	$args['posts_per_page'] = -1; // We need to get all search results for this to work
          	$args['offset'] = 0; // We don't want an offset (ALM handles this)         	
          	
-         	$wp_query = new WP_Query($args);   
+         	$wp_query = new WP_Query( $args );   
          	
-         	// Core Relevanssi Filter   
-         	$wp_query = apply_filters('relevanssi_modify_wp_query', $wp_query);  
+         	// Core Relevanssi Filter.
+         	$wp_query = apply_filters( 'relevanssi_modify_wp_query', $wp_query );  
          	
-         	// Run query 	
-         	relevanssi_do_query($wp_query);  
+         	// Run query. 	
+         	relevanssi_do_query( $wp_query );  
          	
+				// Empty posts array.
          	$posts = [];
          	
          	// Loop all results and pull post IDs
@@ -59,26 +66,27 @@ if(!class_exists('ALM_Relevanssi')) :
          	} 
          	
       		if ( ! empty( $posts ) ) {         		      			      			
-      			$args['post__in'] = $posts; // $relevanssi_query->posts array
-      			$args['orderby'] = 'post__in'; // override orderby to relevance
-      			$args['s'] = ''; // Reset 's' term value
+      			$args['post__in']       = $posts; // $relevanssi_query->posts array
+      			$args['orderby']        = 'post__in'; // Override orderby to relevance
+					$args['search']         = $args['s']; // Set custom `search` arg to pass back to ALM. 
+      			$args['s']              = ''; // Reset 's' term value
       			$args['posts_per_page'] = $old_posts_per_page; // Reset 'posts_per_page' before sending data back
-      			$args['offset'] = $old_offset; // Reset 'offset' before sending data back      			    			
+      			$args['offset']         = $old_offset; // Reset 'offset' before sending data back
+
       		}  
       		    			
       		return $args;	
       	}        	    	
       }
-   }
+   }  
    
-   
-   
-   /*
-   *  ALM_Relevanssi
-   *  The main function responsible for returning the one true ALM_Relevanssi Instance.
-   *
-   *  @since 2.0.0
-   */
+   /**
+    * ALM_Relevanssi
+    * The main function responsible for returning the one true ALM_Relevanssi Instance.
+    *
+	 * @since 1.0
+	 * @author ConnektMedia <darren@connekthq.com>
+    */
    function ALM_Relevanssi(){
       global $ALM_Relevanssi;
       
@@ -88,6 +96,6 @@ if(!class_exists('ALM_Relevanssi')) :
       
       return $ALM_Relevanssi;
    }      
-   ALM_Relevanssi(); // initialize
+   ALM_Relevanssi();
    
 endif;
