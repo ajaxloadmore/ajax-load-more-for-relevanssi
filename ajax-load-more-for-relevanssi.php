ccbbb2c6018bc0fdc8a3395a13a123441c38257c
Darren Cooney <darren.cooney@gmail.com>
2025-06-05 14:54:51 -0400

    Remove activation hooks

--- ajax-load-more-for-relevanssi.php
+++ ajax-load-more-for-relevanssi.php
@@ -18,35 +18,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- *  Installation hook.
- */
-function alm_relevanssi_install() {
-	if ( ! is_plugin_active( 'ajax-load-more/ajax-load-more.php' ) ) {
-		set_transient( 'alm_relevanssi_admin_notice', true, 5 );
-	}
-}
-register_activation_hook( __FILE__, 'alm_relevanssi_install' );
-
-/**
- * Display admin notice if plugin does not meet the requirements.
- */
-function alm_relevanssi_admin_notice() {
-	$slug = 'ajax-load-more';
-	// Ajax Load More Notice.
-	if ( get_transient( 'alm_relevanssi_admin_notice' ) ) {
-		$install_url = get_admin_url() . '/update.php?action=install-plugin&plugin=' . $slug . '&_wpnonce=' . wp_create_nonce( 'install-plugin_' . $slug );
-		$message     = '<div class="error">';
-		$message    .= '<p>You must install and activate the core Ajax Load More plugin before using the Ajax Load More Relevanssi extension.</p>';
-		$message    .= '<p>' . sprintf( '<a href="%s" class="button-primary">%s</a>', $install_url, 'Install Ajax Load More Now' ) . '</p>';
-		$message    .= '</div>';
-		echo wp_kses_post( $message );
-		delete_transient( 'alm_relevanssi_admin_notice' );
-	}
-}
-add_action( 'admin_notices', 'alm_relevanssi_admin_notice' );
-
-
 if ( ! class_exists( 'ALM_Relevanssi' ) ) :
 
 	/**
